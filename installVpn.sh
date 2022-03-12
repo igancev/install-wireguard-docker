47ae81a17fa40996a5475c3645c7f087a05b16fa
Ivan Gantsev <ivangancev@yandex.ru>
2022-03-12 19:21:32 +0300

    add install script

---
+++ installVpn.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+sudo apt update \
+	&& sudo apt upgrade -y \
+	&& sudo apt install docker.io docker-compose -y
+
+mkdir -p ~/wireguard/config
+
+cat << EOF > ~/wireguard/docker-compose.yml
+---
+version: "2.1"
+services:
+  wireguard:
+    image: lscr.io/linuxserver/wireguard
+    container_name: wireguard
+    cap_add:
+      - NET_ADMIN
+      - SYS_MODULE
+    environment:
+      - PUID=1000
+      - PGID=1000
+      - TZ=Europe/London
+      - SERVERURL=auto #optional
+      - SERVERPORT=51820 #optional
+      - PEERS=4 #optional
+      - PEERDNS=auto #optional
+      - INTERNAL_SUBNET=10.13.13.0 #optional
+      - ALLOWEDIPS=0.0.0.0/0 #optional
+    volumes:
+      - ~/wireguard/config:/config
+      - /lib/modules:/lib/modules
+    ports:
+      - 51820:51820/udp
+    sysctls:
+      - net.ipv4.conf.all.src_valid_mark=1
+    restart: unless-stopped
+EOF
+
+docker-compose -f ~/wireguard/docker-compose.yml up -d
+
+docker ps
